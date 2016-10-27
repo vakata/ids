8f9ec57f44031f747ba02ef46b7aec5db13ad5a7
Ivan Bozhanov <git@vakata.com>
2016-10-27 14:39:55 +0300

    fixed docblock

--- src/IDS.php
+++ src/IDS.php
@@ -11,7 +11,6 @@ class IDS
 
     /**
      * Create an instance from a rule file. Rule files follow the Expose / PHPIDS format
-     * @method fromFile
      * @param  string   $path the path to the file to load
      * @return \vakata\ids\IDS         the new instance
      */
@@ -31,7 +30,6 @@ class IDS
     }
     /**
      * Creates an instance from the default rule file that comes with the lib.
-     * @method fromDefaults
      * @return \vakata\ids\IDS       the new instance
      */
     public static function fromDefaults() : IDS
@@ -41,7 +39,6 @@ class IDS
     /**
      * Create an instance.
      * Each rule is and array and must contain `rule` and `impact` keys, and may contain `tags` and `description` keys
-     * @method __construct
      * @param  array       $rules array or rule arrays
      */
     public function __construct(array $rules = [])
@@ -50,7 +47,6 @@ class IDS
     }
     /**
      * Analyze an incoming request
-     * @method analyzeRequest
      * @param  \vakata\http\Request        $req       the request to analyze
      * @param  int|null       $threshold if non-null analysis will stop once this impact number is reached
      * @param  array|null     $tags      if non-null only rules containing any of the supplied tags will be run
@@ -66,7 +62,6 @@ class IDS
     }
     /**
      * Analyze an array of data
-     * @method analyzeData
      * @param  array        $data       the data to analyze
      * @param  int|null       $threshold if non-null analysis will stop once this impact number is reached
      * @param  array|null     $tags      if non-null only rules containing any of the supplied tags will be run
@@ -79,7 +74,6 @@ class IDS
     }
     /**
      * Get the violations from the last analyze call
-     * @method getViolations
      * @return array the violated rules and the values that violated them
      */
     public function getViolations() : array
